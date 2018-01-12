70ff5da07da20799efbee9aa4ce74cdfb3194c3e
Matin Gholami <matingholami@live.com>
2018-01-12 12:21:49 +0330

    get the url from user

--- www/js.js
+++ www/js.js
@@ -1,11 +1,13 @@
 function HReq() {
 	var xmlhttp = new XMLHttpRequest();
+	document.getElementById("time").innerHTML = '';
 	xmlhttp.onreadystatechange = function() {
 			document.getElementById("time").innerHTML += 'ready state:'+this.readyState+'<br>';
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("time").innerHTML += 'status: '+this.status;
+			document.getElementById("time").innerHTML += 'status: '+this.status+'<br />';
+			document.getElementById("time").innerHTML += 'content: '+this.responseText+'<br />';
 		}
 	};
-	xmlhttp.open("GET", "https://gost.ir/time.php", true);
+	xmlhttp.open("GET", document.getElementById("add").value, true);
 	xmlhttp.send();
 }

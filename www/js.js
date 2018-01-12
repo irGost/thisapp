4802fd3c962e0ff005d64d247ca5abb0e7a73548
Matin Gholami <matingholami@live.com>
2018-01-12 16:20:23 +0330

    local storage functions added

--- www/js.js
+++ www/js.js
@@ -11,3 +11,24 @@ function HReq() {
 	xmlhttp.open("GET", document.getElementById("add").value, true);
 	xmlhttp.send();
 }
+var storage = window.localStorage;
+function vars() {
+	document.getElementById("vars").innerHTML = '<input type="button" value="Refresh" onclick="vars()" />';
+	for (var i = 0; i < localStorage.length; i++) {
+		document.getElementById("vars").innerHTML += '<br />'+i+': '+storage.key(i)+' : '+storage.getItem(storage.key(i));
+	}
+}
+function addvar() {
+	storage.setItem(document.getElementById("key").value, document.getElementById("val").value);
+}
+function delvar() {
+	storage.removeItem(storage.key(document.getElementById("idtobedeleted").value));
+}
+function check() {
+	var vall=storage.getItem(storage.key(document.getElementById("keytobechecked").value));
+	if(vall==null){
+		document.getElementById("isset").innerHTML = 'var not set';
+	} else {
+		document.getElementById("isset").innerHTML = vall;
+	}
+}
